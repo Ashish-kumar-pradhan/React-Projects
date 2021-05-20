0b9a4ae14ef4649c77ac02310070ad2744c43b17
Ashish-kumar-pradhan <80833940+Ashish-kumar-pradhan@users.noreply.github.com>
2021-05-20 11:52:09 +0530

    Moding minus

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -20,6 +20,18 @@ class CartItem extends React.Component {
         });
     }
 
+    decreaseQuantity = () => {
+        const{qty} = this.state;
+        if(qty === 0) {
+            return;
+        }
+        this.setState((prevState) => {
+            return{
+            qty: prevState.qty - 1
+            }
+        });
+    }
+
     render() {
         const { price, title, qty } = this.state;
         return (
@@ -41,6 +53,7 @@ class CartItem extends React.Component {
                         <img alt="decrease"
                             className="action-icons"
                             src ="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+                            onClick = {this.decreaseQuantity}
                         />
                         <img alt="delete"
                             className="action-icons"
